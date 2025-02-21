990218b745d7d462f5ac985685023a10aa54688a
catelynbu <catelyn.bu@gmail.com>
2025-02-21 01:14:18 -0800

    fiixning js for every picture

--- everypicture/v2/script.js
+++ everypicture/v2/script.js
@@ -4,32 +4,73 @@
 
     const pic1 = document.querySelector('#polaroid1');
     const pic2 = document.querySelector('#polaroid2');
-    const pic3 = document.querySelector('#polaroid3')
+    const pic3 = document.querySelector('#polaroid3');
 
-    const black = document.querySelector('.black')
+
+    const noblack = document.querySelector('.no-black');
+    const black = document.querySelector('.black');
+
     const fill = document.querySelector('.fill');
 
-    let counter = 0;
+    const bghide = document.querySelector('#cover');
 
+    const close = document.querySelector('button');
+
+    
     pic1.addEventListener('mouseover', function(event){
         event.preventDefault();
-        // pic1.style.border= '3px solid red';
+
+        pic1.style.border= '3px solid red';
+
+        pic1.className = 'shake';
         black.style.opacity = '0';
         black.style.width = '350px';
         fill.style.width = '350px';
 
-        counter = 1;
-        console.log(counter);
+
+        //if the black polaroid is gone then cover the bg and bring up pop-up
+        // do we need the if statement?
+        if( black.style.opacity = '0'){
+            console.log('i think we are onto something');
+            bghide.style.opacity = '1';
+    
+            //eventually the polaroid should stop shaking
+
+            const caption1 = document.querySelector('#caption1');
+
+            caption1.style.display = 'flex';
+
+
+        }
+
     })
 
     pic1.addEventListener('mouseout', function(event){
         event.preventDefault();
-        // black.style.animationPlayState = 'paused';
+
+        pic1.style.border= '3px solid blue';
+
+        pic1.className = 'no-shake';
+        
     })
 
-    if(counter = 1){
-        pic1.style.border= '3px solid red';
-    }
 
-    console.log(`${counter}`)
+    close.addEventListener('click', function(event){
+        event.preventDefault();
+
+        console.log('im begging you nicely');
+
+        //get rid of the cover, change back the size, get rid of pop-up
+        black.style.width = '300px';
+        fill.style.width = '300px';
+        caption1.style.display = 'none';
+        bghide.style.opacity = '0';
+
+    })
+    
+
+
+
+
+
 })();
