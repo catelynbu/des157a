8a91be23425a6f7a49b31b721531320443f815e3
catelynbu <catelyn.bu@gmail.com>
2025-02-25 00:26:28 -0800

    finishing every picture

--- everypicture/v2/script.js
+++ everypicture/v2/script.js
@@ -57,93 +57,18 @@
         
          })
 
+        pic2.addEventListener('mouseout', function(event){
+            console.log('um the cursor is off HELLOO')
+            pic2.className = 'no-shake';
+        
+         })
+
+        pic3.addEventListener('mouseout', function(event){
+            console.log('um the cursor is off HELLOO')
+            pic3.className = 'no-shake';
+        
+         })
+
     }
-
-   
-
-    
-   
-
-
-    // for (let i=0; i<polaroid.length; i++){
-    //     polaroid[i].addEventListener('mouseover', function(event){
-
-    //         if(polaroid[0]){
-    //             console.log('test');
-    //         }
-            
-
-            // console.log('PLEASE WORK')
-
-            //class name might have to change to classlist if we need to keep the polaroid invdividual ids
-            // polaroid[i].className = 'shake';
-            // black[i].style.opacity = '0';
-            // black[i].classList.add('grow');
-            // fill[i].classList.add('grow');
-            
-            // if(polaroid[0].className = 'shake'){
-            //     console.log('the first one is shaking');
-
-            //     // bghide.style.opacity = '1';
-                
-            //     // caption1.style.display = 'flex';
-
-            //     // black[1].classList.remove('grow');
-            //     // fill[1].classList.remove('grow');
-            //     // black[1].classList.add('shrink');
-            //     // fill[1].classList.add('shrink');
-            //     // polaroid[1].classList.remove = ('shake');
-
-            //     // black[2].classList.remove('grow');
-            //     // fill[2].classList.remove('grow');
-            //     // black[2].classList.add('shrink');
-            //     // fill[2].classList.add('shrink');
-
-
-            //     // polaroid[1].style.display = 'none';
-
-            //     // polaroid[0].style.border = 'solid red 3px'
-            //     // polaroid[2].style.opacity = '0';
-
-            //     // if(polaroid[2].style.opacity = '0'){
-            //     //     console.log('poopoo');
-            //     //     polaroid[2].classList.remove('shake');
-            //     //}
-            // }
-            // else if(polaroid[1].className = 'shake'){
-            //     console.log('the second one is shaking');
-            // }
-            // else if(polaroid[2].className = 'shake'){
-            //     console.log('the thrid one is shaking');
-            // }
-
-    //     })
-
-    //     polaroid[i].addEventListener('mouseout', function(event){
-    //         // console.log('um the cursor is off HELLOO')
-
-    //         polaroid[i].className = 'no-shake';
-
-    //     })
-    // }
-
-
-    // close.addEventListener('click', function(event){
-    //     event.preventDefault();
-
-    //     console.log('im begging you nicely');
-
-    //     //get rid of the cover, change back the size, get rid of pop-up
-    //     // black.style.width = '250px';
-    //     // fill.style.width = '250px';
-    //     caption1.style.display = 'none';
-    //     bghide.style.opacity = '0';
-
-    //     //run defualt function. so you need to make a defualt function lmao
-
-    // })
-
-
-
 
 })();
