9aa148c677c65a17c57055a85d7665fe0db9b052
catelynbu <catelyn.bu@gmail.com>
2025-03-13 00:18:50 -0700

    adding audio and fixing player two mechanics

--- game/final/script.js
+++ game/final/script.js
@@ -15,11 +15,54 @@
 
 
     const icons = document.querySelector('#icons');
+    const icons2 = document.querySelector('#icons2');
+    const korila = document.querySelector('.korila');
+    const strawberry = document.querySelector('.straw');
+    const bee = document.querySelectorAll('.bee');
+    const chai = document.querySelectorAll('.chai');
+    const rila = document.querySelector('.rila');
+    const orange = document.querySelector('.orange');
 
-    let counter = 0;
+    const collectSound = new Audio('audio/collectsound.mp3')
+
+
+    const timer = document.querySelectorAll('.timer');
+    let countdownInterval;
+    let time = 10;
+
+     // Start the first round timer
+     function startTimer() {
+        time = 10;  // Reset time
+        countdownInterval = setInterval(function() {
+            time--;
+            updateTimerDisplay(time);
+
+            if (time === 0) {
+                clearInterval(countdownInterval);
+                console.log("Round 1 Time's up!");
+                icons.style.display = 'none';
+                icons2.style.display = 'none';
+                
+                
+            }
+        }, 1000);
+    }
+
+    // Update the timer display
+    function updateTimerDisplay(time) {
+        for(let i=0; i<timer.length; i++){
+             timer[i].innerHTML = time < 10 ? `0:0${time}` : `0:${time}`;
+        }
+       
+    }
+
+    let counter1 = 0;
+    let counter2 = 0;
     const score = document.querySelector('.score');
 
     startButton.addEventListener('click', function(){
+        const bgmusic = new Audio('audio/bgmusic.mp3');
+        bgmusic.play();
         title.style.transition = 'opacity 1s';
         title.style.opacity = 0;
 
@@ -34,125 +77,72 @@
 
             beginRound.style.display = 'flex';
 
-            startGame();
+            startFirstRound();
            
 
         }, 1000)
     });
 
-    function startGame(){
+
+    function startFirstRound(){
         beginButton.addEventListener('click', function(){
             beginRound.style.display = 'none';
             icons.style.display = 'block';
-    
-            const korila = document.querySelector('.korila');
-            const strawberry = document.querySelector('.straw');
-            const bee = document.querySelector('.bee');
-            const chai = document.querySelector('.chai');
-            const rila = document.querySelector('.rila');
-            const orange = document.querySelector('.orange');
-    
-    
-            function spawn(){
-                setInterval(spawnKorila, 2000);
-                setInterval(spawnStrawberry, 1500);
-                setInterval(spawnBee, 2500);
-                setInterval(spawnChai, 2500);
-                setInterval(spawnRila, 2000);
-                setInterval(spawnOrange, 1500);
-                
-    
-    
-                // if (counter > 70){
-                //     setInterval(spawnKorila, 3000);
-                //     setInterval(spawnStrawberry, 2500);
-                //     setInterval(spawnBee, 3500);
-                //     setInterval(spawnChai, 3500);
-                //     console.log('more than 70');
-                // }
+
+            
+            setInterval(spawnRila, 2000);
+            setInterval(spawnOrange, 1500);
+            setInterval(spawnBee, 2500);
+            setInterval(spawnChai, 2500);
+
+            startTimer();
+            
+            setTimeout (function(){
+                nextRound.style.display = 'flex';
+            }, 10000)
+
+           
+            
+            for (let i=0; i<bee.length; i++){
+                bee[i].addEventListener('click', function(){
+                    console.log('bee clicked');
+                    bee[i].style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+                    bee[i].style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            
+                    counter1 = counter1 - 15;
+                    console.log(counter1);
+                    score.innerHTML = `${counter1}`;
+
+                    
+                })
             }
-            spawn();
-    
-            function spawnRila() {
-                rila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                rila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            
+        
+            for (let i=0; i<chai.length; i++){
+                chai[i].addEventListener('click', function(){
+                    console.log('chairoigokuma clicked');
+                    chai[i].style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+                    chai[i].style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            
+                    counter1 = counter1 + 20;
+                    console.log(counter1);
+                    score.innerHTML = `${counter1}`;
+
+                    collectSound.play();
+                })
             }
-        
-            function spawnOrange(){
-                orange.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                orange.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-            }
-    
-            function spawnKorila() {
-                korila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                korila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-            }
-        
-            function spawnStrawberry(){
-                strawberry.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                strawberry.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-            }
-        
-            function spawnBee(){
-                bee.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                bee.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-            }
-        
-            function spawnChai (){
-                chai.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                chai.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-            }
-    
-            korila.addEventListener('click', function(){
-                console.log('korila clicked');
-               
-                korila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                korila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-        
-                counter = counter + 10;
-                console.log(counter);
-                score.innerHTML = `${counter}`;
-            })
-        
-            strawberry.addEventListener('click', function(){
-                console.log('strawberry clicked');
-                strawberry.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                strawberry.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-        
-                counter = counter + 15;
-                console.log(counter);
-                score.innerHTML = `${counter}`;
-            })
-        
-            bee.addEventListener('click', function(){
-                console.log('bee clicked');
-                bee.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                bee.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-        
-                counter = counter - 15;
-                console.log(counter);
-                score.innerHTML = `${counter}`;
-            })
-        
-            chai.addEventListener('click', function(){
-                console.log('chairoigokuma clicked');
-                chai.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-                chai.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
-        
-                counter = counter + 20;
-                console.log(counter);
-                score.innerHTML = `${counter}`;
-            })
     
             rila.addEventListener('click', function(){
                 console.log('rila clicked');
-               
+                
                 rila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
                 rila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
         
-                counter = counter + 10;
-                console.log(counter);
-                score.innerHTML = `${counter}`;
+                counter1 = counter1 + 10;
+                console.log(counter1);
+                score.innerHTML = `${counter1}`;
+
+                collectSound.play();
             })
         
             orange.addEventListener('click', function(){
@@ -160,38 +150,14 @@
                 orange.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
                 orange.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
         
-                counter = counter + 15;
-                console.log(counter);
-                score.innerHTML = `${counter}`;
+                counter1 = counter1 + 15;
+                console.log(counter1);
+                score.innerHTML = `${counter1}`;
+
+                collectSound.play();
             })
-    
-    
-            const timer = document.querySelector('.timer');
-    
-            let time = 30;
-            const countdownTimer = setInterval(function() {
-                console.log(time);
-                time--;
-    
-                timer.innerHTML = '0:' + time;
-    
-                if (time < 10){
-                    timer.innerHTML = '0:0' + time;
-                }
-    
-                if (time === 0) {
-                    clearInterval(countdownTimer);
-                    console.log("Time's up!");
-    
-                    icons.style.display = 'none';
-                    
-                    nextRound.style.display = 'flex';
-                }
-            }, 1000);
-    
+           
         })
-    
-        
     }
 
     nextButton.addEventListener('click', function(){
@@ -202,18 +168,55 @@
                 korilaSidebar.style.width = '25vw';
                 background.style.backgroundImage = 'url(images/korila-bg.jpg)';
                 nextRound.style.display = 'none';
-                beginRound.style.display = 'flex';
-
-                startGame();
+                // beginRound.style.display = 'flex';
+                
             }, 350)
-
-        
 
         }, 1000)
 
+        setTimeout(function(){
+            icons2.style.display = 'block';
+            setInterval(spawnKorila, 2000);
+            setInterval(spawnStrawberry, 1500);
+            setInterval(spawnBee, 2500);
+            setInterval(spawnChai, 2500);
+
+            clearInterval(countdownInterval);  // Clear previous timer interval
+            startTimer();
+
+           
+
+            korila.addEventListener('click', function(){
+                console.log('korila clicked');
+               
+                korila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+                korila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+        
+                counter1 = counter1 + 10;
+                console.log(counter1);
+                score.innerHTML = `${counter1}`;
+
+                collectSound.play();
+            })
+        
+            strawberry.addEventListener('click', function(){
+                console.log('strawberry clicked');
+                strawberry.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+                strawberry.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+        
+                counter1 = counter1 + 15;
+                console.log(counter1);
+                score.innerHTML = `${counter1}`;
+
+                collectSound.play();
+            })
+
+        }, 2000)
+
+        
+        
 
     })
-    
 
 
     function rilaSide (){
@@ -234,5 +237,105 @@
             korilaSidebar.style.backgroundColor = '#C39A9A'
         }, 300)
     }
+
+    function spawnRila() {
+        rila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+        rila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    }
+
+    function spawnOrange(){
+        orange.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+        orange.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    }
+
+    function spawnKorila() {
+        korila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+        korila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    }
+
+    function spawnStrawberry(){
+        strawberry.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+        strawberry.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    }
+
+    function spawnBee(){
+        for (let i=0; i<bee.length; i++){
+            bee[i].style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            bee[i].style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+        }
+        
+    }
+
+    function spawnChai (){
+        for (let i=0; i<chai.length; i++){
+            chai.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            chai.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+        }
+        
+    }
+
+    function spawnsClick(){
+        korila.addEventListener('click', function(){
+            console.log('korila clicked');
+           
+            korila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            korila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
     
+            counter1 = counter1 + 10;
+            console.log(counter1);
+            score.innerHTML = `${counter1}`;
+        })
+    
+        strawberry.addEventListener('click', function(){
+            console.log('strawberry clicked');
+            strawberry.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            strawberry.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    
+            counter1 = counter1 + 15;
+            console.log(counter1);
+            score.innerHTML = `${counter1}`;
+        })
+    
+        bee.addEventListener('click', function(){
+            console.log('bee clicked');
+            bee.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            bee.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    
+            counter1 = counter1 - 15;
+            console.log(counter1);
+            score.innerHTML = `${counter1}`;
+        })
+    
+        chai.addEventListener('click', function(){
+            console.log('chairoigokuma clicked');
+            chai.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            chai.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    
+            counter1 = counter1 + 20;
+            console.log(counter1);
+            score.innerHTML = `${counter1}`;
+        })
+
+        rila.addEventListener('click', function(){
+            console.log('rila clicked');
+           
+            rila.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            rila.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    
+            counter1 = counter1 + 10;
+            console.log(counter1);
+            score.innerHTML = `${counter1}`;
+        })
+    
+        orange.addEventListener('click', function(){
+            console.log('orange clicked');
+            orange.style.left = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+            orange.style.bottom = `${(Math.floor(Math.random()*(610-0)+1))}px`;
+    
+            counter1 = counter1 + 15;
+            console.log(counter1);
+            score.innerHTML = `${counter1}`;
+        })
+    }
+
 })();
